b620c28ba14b2d511ac6f7a1a0aa96aa87d8e195
hudayberdyyev <hudayberdyyev.ahmet@gmail.com>
2023-06-27 17:07:22 +0500

    logging in both requests are implemented

--- pkg/handler/checkIp.go
+++ pkg/handler/checkIp.go
@@ -10,6 +10,7 @@ func (h *Handler) checkIP(w http.ResponseWriter, r *http.Request) {
 	// Simple authorization middleware
 	if r.Header.Get(AuthorizationHeader) != h.secretKey {
 		http.Error(w, "invalid secret key", http.StatusBadRequest)
+		log.Printf("invalid secret key, incoming header = %s\n", h.secretKey)
 		return
 	}
 
@@ -17,6 +18,7 @@ func (h *Handler) checkIP(w http.ResponseWriter, r *http.Request) {
 	var checkIpRequest CheckIpBody
 	err := json.NewDecoder(r.Body).Decode(&checkIpRequest)
 	if err != nil {
+		log.Printf("error when parse check ip request body: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -27,22 +29,27 @@ func (h *Handler) checkIP(w http.ResponseWriter, r *http.Request) {
 		// Just log it
 		err := h.repo.IWriter.SaveCheckIpLogToDB(checkIpRequest.GetCheckIpModel())
 		if err != nil {
-			log.Println(err.Error())
+			log.Printf("error when save check ip request log to DB: %v\n", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	case AccessDenied:
 		// Compare it from with access on db
 		userId := checkIpRequest.GetUserIdFromAccessToken()
 		userAccess, err := h.repo.IReader.GetAccessStatusFromUsersDB(userId)
 		if err != nil {
-			log.Println(err.Error())
+			log.Printf("%s\n", err.Error())
+			return
 		}
 		ipAccess, err := h.repo.IReader.GetAccessStatusFromHosts(checkIpRequest.XForwardedForHeader)
 		if err != nil {
-			log.Println(err.Error())
+			log.Printf("%s\n", err.Error())
 		}
 		err = h.repo.IWriter.SaveCheckIpLogToDB(checkIpRequest.GetCheckIpModelWithArgs(ipAccess, userAccess))
 		if err != nil {
-			log.Println(err.Error())
+			log.Printf("error when save check ip request log to DB: %v\n", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	default:
 		log.Printf("checkIp access status value equal to = %d\n", checkIpRequest.ResponseIsAccessAllowed)
--- pkg/handler/refresh_token.go
+++ pkg/handler/refresh_token.go
@@ -2,12 +2,14 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func (h *Handler) refreshToken(w http.ResponseWriter, r *http.Request) {
 	// Simple authorization middleware
 	if r.Header.Get(AuthorizationHeader) != h.secretKey {
+		log.Printf("authorization header doesn't match, incoming header = %s\n", h.secretKey)
 		http.Error(w, "invalid secret key", http.StatusBadRequest)
 		return
 	}
@@ -16,12 +18,14 @@ func (h *Handler) refreshToken(w http.ResponseWriter, r *http.Request) {
 	var refreshTokenRequest RefreshTokenBody
 	err := json.NewDecoder(r.Body).Decode(&refreshTokenRequest)
 	if err != nil {
+		log.Printf("can't parse refresh token body: %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	if err := h.repo.IWriter.SaveRefreshTokenToDB(refreshTokenRequest.GetRefreshTokenDbModel()); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Printf("error when save refresh token request log to DB: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
